72b302ae4fc0d576f9bd8a3819c98dc8fae20150
David Lawrence Miller <github@ninepointeightone.net>
2014-05-20 16:15:56 -0400

    junk data simulation working, need to run it...

---
+++ junkdata/README.md
@@ -0,0 +1,5 @@
+# Junk data simulations
+
+This folder contains simulation code, results and analysis for the case in which "junk" covariates are added to the model. These covariates don't affect detectability, but have been recorded anyway for some reason.
+
+
--- junkdata/sim.R
+++ junkdata/sim.R
@@ -10,8 +10,7 @@ source("sim.hn.R")
 options(cores=2)
 registerDoMC()
 
-
-
+# function to generate data
 make.data <- function(sample.size,width,pars,covdata=NULL,formula=~1){
 
   if(formula != "~1"){
@@ -23,7 +22,6 @@ make.data <- function(sample.size,width,pars,covdata=NULL,formula=~1){
   # simulate some data
   ddat <- sim.hn(sample.size,z, pars, width)
 
-
   # build a data.frame
   if(is.null(covdata)){
     dat <- data.frame(distance=ddat)
@@ -34,17 +32,32 @@ make.data <- function(sample.size,width,pars,covdata=NULL,formula=~1){
   return(dat)
 }
 
+# shortcut function to store results
+store_results <- function(mod, model.name, sample.size, sim, res){
+  # mod         - fitted model object (call was wrapped in a try())
+  # model.name  - string to identify model
+  # sample.size - number of observations
+  # sim         - simulation id
+  # results     - data.frame to append to
+  if(class(mod)!="try-error" | !is.null(mod$model)){
+    p <- sample.size/mod$ddf$Nhat
+  }else{
+    p <- NA
+  }
+  res <- rbind(res,
+               c(sim, sample.size, model.name, sample.size/mod$ddf$Nhat))
+  return(res)
+}
+
 ## options
 # number of simulations to do per sample size
-n.sims <- 1000
+n.sims <- 200
 # number of distances observed
 n.samps <- c(30,60,120,240,480)
 # truncation
 width <- 0.75
 #parameters
 pars <- log(0.3)
-# what is the covariate we're including?
-covdata <- data.frame(cov1=runif(sample.size,-1,1))
 # include the covariate name in the formula to have it be non-spurious
 formula <- ~1
 
@@ -58,52 +71,33 @@ for(sample.size in n.samps){
   results <- foreach(sim = 1:n.sims, .combine=rbind,
                    .inorder=FALSE, .init=c()) %dopar% {
 
-    # generate data
+    res <- c()
+
+    ## generate data
+    # what is the covariate we're including?
+    covdata <- data.frame(cov1=runif(sample.size,-1,1))
+    # make the distance data frame
     this.data <- make.data(sample.size,width,pars,covdata,formula)
 
     # fit the "true" model -- model data was generated from
     mod1 <- try(ds(this.data,truncation=width))
+    res <- store_results(mod1, "hn", sample.size, sim, res)
 
-    if(class(mod1)!="try-error"){
-      res <- c(sim, sample.size, "hn", sample.size/mod1$ddf$Nhat)
-    }else{
-      res <- c(sim, sample.size, "hn", NA)
-    }
-
-    # fit with "wrong" models...
+    ## fit with "wrong" models...
 
     # hn with covariate
-    mod1.cov <- ds(this.data,truncation=width,formula=~cov1)
-    if(class(mod1.cov)!="try-error"){
-      res <- rbind(res,
-                   c(sim, "hn+cov1", sample.size/mod1.cov$ddf$Nhat))
-    }else{
-      res <- rbind(res,
-                   c(sim, "hn+cov1", NA))
-    }
+    mod1.cov <- try(ds(this.data,truncation=width,formula=~cov1))
+    res <- store_results(mod1.cov, "hn+cov1", sample.size, sim, res)
 
     # hazard rate
-    mod1.hr <- ds(this.data,truncation=width,key="hr")
-    if(class(mod1.hr)!="try-error"){
-      res <- rbind(res,
-                   c(sim, "hr", sample.size/mod1.hr$ddf$Nhat))
-    }else{
-      res <- rbind(res,
-                   c(sim, "hr", NA))
-    }
+    mod1.hr <- try(ds(this.data,truncation=width,key="hr"))
+    res <- store_results(mod1.hr, "hr", sample.size, sim, res)
 
     # hazard rate with covariate
-    mod1.hr.cov <- ds(this.data,truncation=width,formula=~cov1,key="hr")
-    if(class(mod1.hr.cov)!="try-error"){
-      res <- rbind(res,
-                   c(sim, "hr+cov1", sample.size/mod1.hr.cov$ddf$Nhat))
-    }else{
-      res <- rbind(res,
-                   c(sim, "hr+cov1", NA))
-    }
+    mod1.hr.cov <- try(ds(this.data,truncation=width,formula=~cov1,key="hr"))
+    res <- store_results(mod1.hr.cov, "hr+cov1", sample.size, sim, res)
 
     return(res)
-
   }
 
   big.res <- rbind(big.res,results)
@@ -113,12 +107,17 @@ write.csv(big.res, file="junk.csv")
 
 
 ## quick plot
-#results <- as.data.frame(results)
-#names(results) <- c("sim","model","p")
-#library(ggplot2)
-#p <- ggplot(results)
-#p <- p + geom_boxplot(aes(x=model,y=p))
-#print(p)
+# load data here 
+results <- as.data.frame(big.res)
+names(results) <- c("sim","n","model","p")
+results$p <- as.numeric(as.character(results$p))
+results$n <- as.factor(as.numeric(as.character(results$n)))
+library(ggplot2)
+p <- ggplot(results)
+p <- p + geom_boxplot(aes(x=n,y=p))
+p <- p + facet_wrap(~model)
+p <- p + scale_y_continuous(limits=c(0,1))
+print(p)
 
 
 
